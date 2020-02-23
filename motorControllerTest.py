01325dba873c768e3f4a9876675a7c61045b5510
Eric Boehlke <eric.p.boehlke@gmail.com>
2020-02-22 21:42:38 -0500

    updated  motor test

--- motorControllerTest.py
+++ motorControllerTest.py
@@ -1,8 +1,8 @@
 import wiringpi
 import time
 
-rightMotorDir = 28
-leftMotorDir = 29
+rightMotorDir = 29
+leftMotorDir = 28
 rightMotorSpeed = 23
 leftMotorSpeed = 26 
 
@@ -15,10 +15,25 @@ wiringpi.pinMode(leftMotorSpeed, 1)
 wiringpi.softPwmCreate(rightMotorSpeed, 0, 100)
 wiringpi.softPwmCreate(leftMotorSpeed, 0, 100)
 
-# Start with low power
+# Start with low power for 3 seconds
+wiringpi.digitalWrite(rightMotorDir, 1)
+wiringpi.digitalWrite(rightMotorDir, 1)
+  
 wiringpi.softPwmWrite(rightMotorSpeed, 10) 
 wiringpi.softPwmWrite(leftMotorSpeed, 10) 
 
+time.sleep(3)
+
+# Switch direction
+wiringpi.digitalWrite(rightMotorDir, 0)
+wiringpi.digitalWrite(leftMotorDir, 0)
+
+
+time.sleep(3)
+
+
+wiringpi.softPwmWrite(rightMotorSpeed, 0) 
+wiringpi.softPwmWrite(leftMotorSpeed, 0) 
 
 while True:
 	time.sleep(1)
